52437c28d6931d2280f1b7c1863193c61978d929
Rishabh Kalra <rishabhkalra1501@gmail.com>
2021-07-09 15:28:36 +0530

    added copyright and documentations

--- Logging/logger.py
+++ Logging/logger.py
@@ -1,27 +1,82 @@
+"""
+Copyright (c) 2021 Rishabh Kalra <rishabhkalra1501@gmail.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+"""
+
+#importing libraries
 from datetime import datetime
 
-
 class Logging:
-
+    """
+    Keyword arguments: None
+    argument -- file_path : location of the log file
+    Return: None 
+    """
+    
+    
     def __init__(self, file_path):
         self.file_path = file_path
 
+    def __repr__(self):
+        return f"Logging({self.file_path})"
+
     def log(self, message, log_type):
+        """
+        custom log method
+        Args:
+            message -> str: message to be logged
+            log_type -> str: type of message to be logged ,for example = ("info","error", etc..)
+        """   
+             
         date_time = datetime.now().strftime("%d/%m/%Y, %H:%M:%S")
         with open(self.file_path, "a+") as f:
             f.write(date_time + " -> " + log_type + ": " + message + "\n")
 
     def info(self, message, log_type="INFO"):
+        """
+        Info log method
+        Args:
+            message -> str: message to be logged
+            log_type -> str: INFO 
+        """   
         date_time = datetime.now().strftime("%d/%m/%Y, %H:%M:%S")
         with open(self.file_path, "a+") as f:
             f.write(date_time + " -> " + log_type + ": " + message + "\n")
 
     def warning(self, message, log_type="WARNING"):
+        """
+        Warning log method
+        Args:
+            message -> str: message to be logged
+            log_type -> str: WARNING 
+        """   
         date_time = datetime.now().strftime("%d/%m/%Y, %H:%M:%S")
         with open(self.file_path, "a+") as f:
             f.write(date_time + " -> " + log_type + ": " + message + "\n")
 
     def error(self, message, log_type="ERROR"):
+        """
+        Error log method
+        Args:
+            message -> str: message to be logged
+            log_type -> str: ERROR 
+        """   
         date_time = datetime.now().strftime("%d/%m/%Y, %H:%M:%S")
         with open(self.file_path, "a+") as f:
             f.write(date_time + " -> " + log_type + ": " + message + "\n")
--- data_import/data_input.py
+++ data_import/data_input.py
@@ -1,14 +1,62 @@
+"""
+Copyright (c) 2021 Rishabh Kalra <rishabhkalra1501@gmail.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+"""
+
+#importing libraries
 import os
 import pandas as pd
 from Logging.logger import Logging
 
 
 class DataInput:
+    """    
+    Class to take input of CSV files for training and prediction
+    
+    Keyword arguments: log_folder_name="Training_Logs", log_file_name="1-file_input.txt"
+    
+    argument -- 
+    log_folder_name: Specifies the folder for Training Logs
+    log_file_name: Specifies the name of the log file
+    
+    Return: None
+    """   
 
     def __init__(self, log_folder_name="Training_Logs", log_file_name="1-file_input.txt"):
         self.log = Logging(os.path.join(log_folder_name, log_file_name))
 
-    def ret_dataframe(self, file_path, rows=None, drop_null=True):
+    def ret_dataframe(self, file_path:"str", rows:"int"=None, drop_null:"bool"=True) -> "pd.DataFrame":
+        """
+        returns a DataFrame after reading data from a given CSV file
+        
+        Args:
+            file_path -> str/path : path of the CSV file.
+            rows (int, optional): numbers of rows to read. Defaults to None: reads all rows.
+            drop_null (bool, optional):Whether to include or exclude null_values in DataFrame. Defaults to True.
+
+        Raises:
+            OSError: path not correct
+            Exception: any other Exception
+
+        Returns:
+            pandas.DataFrame: pandas DataFrame
+        """
         try:
             df = pd.read_csv(file_path, nrows=rows)
             self.log.info("DataFrame created successfully!")
@@ -25,7 +73,7 @@ class DataInput:
             elif not null_check:
                 self.log.info("No Null Values Present!")
 
-            return df
+            return df #returning the DataFrame
 
         except OSError as e:
             self.log.error(f"File Not Found!! function ret_dataframe: {e}")
